002011d5f453c5e0121c0bfc39537d038d1ab4b3
Muhammed Emin <meminkbey@gmail.com>
2025-12-16 22:49:14 +0300

    feat: OOP, Layers, Single Responsibility (Solid)

---
+++ backend/CSharp_Mastery_OOP/01_EntityLayer/Concrete/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_EntityLayer.Concrete
+{
+    internal class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerSurname { get; set; }
+        public string CustomerDistrict { get; set; }
+        public string CustomerCity { get; set; }
+    }
+}
---
+++ backend/CSharp_Mastery_OOP/01_EntityLayer/Concrete/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_EntityLayer.Concrete
+{
+    internal class Order
+    {
+        public int OrderId { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
